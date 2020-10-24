7c74185b117c9f612f8d4140013675823765b19f
rifath33 <hrifath20@stuy.edu>
2020-10-24 13:18:55 -0400

    Yay. I finally got arrayToString and arrayDeepToString working!

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,5 +1,59 @@
 public class ArrayOps{
 
 
+  public static String arrToString(int[] arr){
+
+    String addedArray = "";
+    String result = "";
+
+    for(int i=0; i < arr.length; i++){
+
+      if(i != arr.length - 1)
+      result = result + arr[i] + ", ";
+
+      if(i == arr.length - 1)
+      result = result + arr[i];
+
+    }
+
+    addedArray = "{" + result + "}";
+
+    return addedArray;
+
+  }
+
+  public static String arrDeepToString(int[][] arr) {
+
+    String result = "";
+
+    for(int f = 0; f < arr.length; f++){
+
+      if(f != arr.length-1)
+      result = result + ArrayOps.arrToString(arr[f]) + ", ";
+
+      if(f == arr.length-1)
+      result = result + ArrayOps.arrToString(arr[f]);
+
+    }
+
+    return "{" + result + "}";
+
+    }
 
 }
+
+
+// code that doesn't do what we want... ugh
+/*
+    for(int f = 0; f < arr.length; f++){
+
+      for(int i=0; i < arr[f].length; i++){
+
+        if( (i == arr[f].length - 1) && (f == arr.length - 1) )
+        result = result + arr[f][i];
+        else result = result + arr[f][i] + ", ";
+
+        }
+
+      }
+*/
--- Tester.java
+++ Tester.java
@@ -3,5 +3,13 @@ public class Tester{
 
     // test cases and print statements
 
+    int[][] arr = { {2,4,6}, {8,9,10}, {15,16,19} };
+
+    System.out.println(ArrayOps.arrDeepToString(arr));
+
+    int[][] kut = { {1,2,3}, {45,76,21}, {43,65,12}, {75,1,5}, {2,3,4,5,6}, {7,8,8,10} };
+
+    System.out.println(ArrayOps.arrDeepToString(kut));
+
   }
 }
