d41e5d55b56d194955edd5fccc19eab1f9d4f4af
rifath33 <hrifath20@stuy.edu>
2020-10-26 08:33:05 -0400

    Got 4c to work

--- Tester.java
+++ Tester.java
@@ -1,11 +1,16 @@
+import java.util.Arrays;
+
 public class Tester{
   public static void main(String[]args){
 
-    int[] kutha = {1,5,10,1,0};
+    int[][] kutha = { {1,2,3}, {7,8,9}, {2,3} };
 
-    System.out.println(ArrayOps.largest(
-    kutha
-    ));
+    System.out.println(
+    Arrays.toString
+    (
+    ArrayOps.sumRows(kutha)
+    )
+    );
 
   }
 }
