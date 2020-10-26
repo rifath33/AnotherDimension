43755910554f9f80054acb772417490e37d1b41d
rifath33 <hrifath20@stuy.edu>
2020-10-26 07:08:21 -0400

    Got 4a and 4b working

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,5 +1,38 @@
 public class ArrayOps{
 
-  
+  /*
+  public static int sum(int[] arr)
+  public static  int largest(int[]arr)
+  public static int[] sumRows(int[][] matrix)
+  public static  int[] largestInRows(int[][] matrix)
+  public static  int sum(int[][] arr)
+  public static int[] sumCols(int[][] matrix)
+  public static boolean isRowMagic(int[][] matrix)
+  public static boolean isColMagic(int[][] matrix)
+  public static boolean isLocationMagic(int[][] matrix, int row, int col)
+  */
+
+  // sum() takes an array of int, and returns a sum of the individual
+  // values in the array.
+
+  public static int sum(int[] arr){
+    int result = 0;
+    for(int i = 0; i < arr.length; i++){
+      result = result + arr[i];
+    }
+    return result;
+  }
+
+  // largest() takes a non-empty array of int, and returns the largest
+  // element of the array.
+
+  public static int largest(int[] arr){
+    int result = arr[0];
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] > result)
+      result = arr[i];
+    }
+    return result;
+  }
 
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,11 @@
 public class Tester{
   public static void main(String[]args){
-    
+
+    int[] kutha = {1,5,10,1,0};
+
+    System.out.println(ArrayOps.largest(
+    kutha
+    ));
+
   }
 }
